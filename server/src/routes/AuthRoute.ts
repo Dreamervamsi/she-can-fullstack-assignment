2fbfa79610ccf5663384b25ecd3446cc76359726
vamsi krishna <kvamsi.nellore@gmail.com>
2026-05-28 16:22:54 +0530

    update tsconfig file

--- server/src/routes/AuthRoute.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { loginUser, registerUser, getProfile } from '../controllers/AuthController';
-import { requireAuth } from '../middlewares/authMiddleware';
-
-export const authRouter = Router();
-
-authRouter.post('/register', registerUser);
-authRouter.post('/login', loginUser);
-authRouter.get('/profile', requireAuth, getProfile);
